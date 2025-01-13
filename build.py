55d0807ee1483462dd0376bfefd8ed85d6723d08
exoad <jackmeng0814@gmail.com>
2025-01-13 13:29:47 -0500

    revise the build script for generating a comprehensive release oriented artifact

--- build.py
+++ build.py
@@ -2,6 +2,7 @@
 
 # Default build file used
 import os
+import platform
 import sys
 import logging
 import subprocess
@@ -10,9 +11,7 @@ import shutil
 import traceback
 
 # configuration stuffs (change if you want to)
-BUILD_OUTPUT_FOLDER_NAME = "BuildArtifacts"
-BUILD_OUTPUT_FOLDER_ROOT_NAME = "OnTheFly"
-BUILD_OUTPUT_FOLDER_UPDATER_NAME = "OnTheFly_Updater"
+BUILD_OUTPUT_FOLDER_NAME = "Artifacts"
 ALLOW_BUILD_LOG_TO_FILE = True
 BUILD_LOG_FILE_NAME = (
     "onthefly_pybuilder.log"  # only is used if ALLOW_BUILD_LOG_TO_FILE is also True
@@ -23,21 +22,15 @@ logger = logging.getLogger("OnTheFly_PyBuilder")
 release_build = False
 target_platform = "windows"
 ephemeral_build_root_folder = None
+launcher_executable_name = "onthefly_launcher"
 
 
 # build processing subroutines
 def init_build_folder():
     logger.info("INIT_BUILD_FOLDER")
     try:
-        os.makedirs(
-            os.path.join(BUILD_OUTPUT_FOLDER_NAME, BUILD_OUTPUT_FOLDER_ROOT_NAME)
-        )
-        os.mkdir(
-            os.path.join(BUILD_OUTPUT_FOLDER_NAME, BUILD_OUTPUT_FOLDER_UPDATER_NAME)
-        )
-        logger.info(
-            f"BUILD_FOLDER @@ {BUILD_OUTPUT_FOLDER_NAME}/{BUILD_OUTPUT_FOLDER_UPDATER_NAME} and {BUILD_OUTPUT_FOLDER_NAME}/{BUILD_OUTPUT_FOLDER_ROOT_NAME}"
-        )
+        os.makedirs(BUILD_OUTPUT_FOLDER_NAME)
+        logger.info(f"BUILD_FOLDER @@ {BUILD_OUTPUT_FOLDER_NAME}")
     except FileExistsError:
         logger.warning("BUILD_FOLDER @@ Already exists, skipping...")
     except PermissionError:
@@ -46,24 +39,53 @@ def init_build_folder():
         logger.error(f"BUILD_FOLDER @@ Error occured while initting {e}")
 
 
-def run_flutter_build(curr_dir: str | None = None) -> int:
-    logger.info(f"RUN_FLUTTER_BUILD @@ {curr_dir}")
-    if not curr_dir:
-        curr_dir = "."
+def __run_cmd__(
+    name: str,
+    curr_dir: str,
+    invokes: list[str],
+    std_err: int = subprocess.STDOUT,
+    std_out: int = subprocess.PIPE,
+) -> int:
+    logger.info(f"{name} @@ {curr_dir}")
     start_build_time: float = time.time()
-    logger.info(f"FLUTTER_BUILD invoked at {time.ctime(start_build_time)}")
-    flutter_build: subprocess.CompletedProcess[str] = subprocess.run(
-        ["flutter.bat", "build", target_platform, "--release" if release_build else ""],
+    logger.info(f"{name} invoked at {time.ctime(start_build_time)}")
+    build_context: subprocess.CompletedProcess[str] = subprocess.run(
+        invokes,
         cwd=os.path.join(os.getcwd(), curr_dir),
-        stderr=subprocess.STDOUT,
-        stdout=subprocess.PIPE,
+        stderr=std_err,
+        stdout=std_out,
         text=True,
     )
     # might need to finish some encoding issues with the output
     logger.info(
-        f"FLUTTER_BUILD @@ TOOK: {time.time()-start_build_time} {curr_dir}\nOUTPUT [{flutter_build.returncode}]\n{flutter_build.stdout}"
+        f"{name} @@ TOOK: {time.time()-start_build_time} {curr_dir}\nOUTPUT [{build_context.returncode}]\n{build_context.stdout}"
     )
-    return flutter_build.returncode
+    return build_context.returncode
+
+
+def run_make(curr_dir: str | None, invokes: list[str] | None = None) -> int:
+    def_invoke: list[str] = ["make"]
+    return __run_cmd__(
+        "MAKE",
+        curr_dir=curr_dir if curr_dir else ".",
+        invokes=def_invoke if not invokes else def_invoke + invokes,
+    )
+
+
+def run_flutter_build(
+    curr_dir: str | None = None, invokes: list[str] | None = None
+) -> int:
+    def_invokes: list[str] = [
+        "flutter.bat",
+        "build",
+        target_platform,
+        "--release" if release_build else "",
+    ]
+    return __run_cmd__(
+        "FLUTTER_BUILD",
+        curr_dir=curr_dir if curr_dir else ".",
+        invokes=def_invokes if not invokes else def_invokes + invokes,
+    )
 
 
 def mv_cpyfolders(src: str, dest: str, force_move=False, cleanup=True):
@@ -127,12 +149,32 @@ if __name__ == "__main__":
         logger.error(f"{traceback.format_exc()}")
     mv_cpyfolders(
         ephemeral_build_root_folder,
-        os.path.join(BUILD_OUTPUT_FOLDER_NAME, BUILD_OUTPUT_FOLDER_ROOT_NAME),
+        os.path.join(BUILD_OUTPUT_FOLDER_NAME),
     )
+    # section for building the launcher program
+    logger.info("Start build for launcher")
+    launcher_build_root_folder = os.path.join("launcher", "dist")
+    try:
+        if (
+            code := run_make(
+                "./launcher",
+                invokes=[
+                    "build_release",
+                    f"o_name={launcher_executable_name}",
+                ],
+            )
+        ) != 0:
+            raise RuntimeError(f"Misconfigured launcher MAKE!!! {code}")
+        else:
+            logger.info("make [./launcher] OK")
+    except Exception:
+        logger.error(f"{traceback.format_exc()}")
+    mv_cpyfolders(
+        launcher_build_root_folder,
+        os.path.join(BUILD_OUTPUT_FOLDER_NAME),
+    )
+    logger.info("============= END =============")
     end_build_time = time.time()
     logger.info(
         f"Build Ended at {time.ctime(end_build_time)}, took {end_build_time - start_build_time}s"
     )
-    # section for building the updater program
-
-    logger.info("============= END =============")
